fba8d52a203ca05f3a939625c5d75d42aaaa4118
Ganeshamoorthi28 <ganesha.moorthi2809@gmail.com>
2022-02-14 20:10:11 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let port = 3000 || process.env.PORT 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+let workItems = [];
+let items = [];
+
+app.get("/", function (req, res) {
+  today = new Date();
+  let options = {
+    weekday: "long",
+    // year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+
+  let day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("work");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.listen(port, function () {
+  console.log("Server Started");
+});
